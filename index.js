228ce0f7cb52e321f098fcba66cab2cc50f8d18f
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-10-08 12:43:14 -0300

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 let message = "";
 
-const pokedex = [];
+const book = [];
 
 app.set("view engine", "ejs");
 
@@ -14,34 +14,40 @@ app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-
-    setTimeout(() => {
-        message = "";
-      }, 1000);
-
-    res.render("index", {});
+  res.render("index", {});
 });
 
-// app.get("/cadastro", (req, res) => {
-//     res.render("cadastro");
-// });
-  
-// app.get("/detalhes/:id", (req, res) => {
-//     const id = req.params.id;
-//     const pokemon = pokedex[id];
-//     res.render("detalhes", {
-//         pokemon,
-//     });
-// });
+app.get("/detalhes/:id", (req, res) => {
+    const id = req.params.id;
+    const bookDaGalera = book[id];
+    res.render("detalhes", {
+      bookDaGalera,
+    });
+});
 
-// app.post("/new", (req, res) => {
-//     const pokemon = req.body;
-//     pokedex.push(pokemon);
-//     message = "Pokemon criado com sucesso!";
-//     res.redirect("/");
-//   });
+app.get("/sobre", (req, res) => {
+  res.render("sobre");
+});
 
-  app.listen(port, () =>
-  console.log(`Servidor rodando em http://localhost:${port}`)
+app.get("/membros", (req, res) => {
+  setTimeout(() => {
+    message = "";
+  }, 1000);
+
+  res.render("membros", {
+    message,
+  });
+});
+
+app.post("/new", (req, res) => {
+  const pokemon = req.body;
+  book.push(bookDaGalera);
+  message = "Informações adiconadas";
+  res.redirect("/mebros");
+});
+
+
+app.listen(port, () =>
+console.log(`Servidor rodando em http://localhost:${port}`)
 );
 
