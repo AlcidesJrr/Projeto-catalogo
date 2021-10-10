3e6947b77eeef9505f96075e51609bf1a0b42a64
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-10-10 02:48:06 -0300

    commit

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 let message = "";
-const book = [];
+const aniversarios = [];
+const baladas = [];
+const casamentos = [];
+const churrascos = [];
+const festivais = [];
+const happyHour = [];
+const malabares = [];
+const reveillons = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index", {});
-});
 
-app.get("/sobre", (req, res) => {
-  res.render("sobre");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.get("/controle", (req, res) => {
-
   setTimeout(() => {
     message = "";
   }, 1000);
@@ -35,10 +38,65 @@ app.get("/membros", (req, res) => {
   res.render("membros");
 });
 
+app.get("/sobre", (req, res) => {
+  res.render("sobre");
+});
+
+app.get("/eventos", (req, res) => {
+  res.render("eventos");
+});
+
+app.get("/aniversarios", (req, res) => {
+  res.render("aniversarios", {
+      aniversarios: aniversarios
+  });
+});
+
+app.get("/baladas", (req, res) => {
+  res.render("baladas", {
+    baladas: baladas
+});
+});
+
+app.get("/casamentos", (req, res) => {
+  res.render("casamentos", {
+    casamentos: casamentos
+});
+});
+
+app.get("/churrascos", (req, res) => {
+  res.render("churrascos",{
+    churrascos: churrascos
+});
+});
+
+app.get("/festivais", (req, res) => {
+  res.render("festivais", {
+    festivais: festivais
+});
+});
+
+app.get("/happyHour", (req, res) => {
+  res.render("happyHour", {
+    happyHour: happyHour
+});
+});
+
+app.get("/malabares", (req, res) => {
+  res.render("malabares", {
+    malabares: malabares
+});
+});
+
+app.get("/reveillons", (req, res) => {
+  res.render("reveillons", {
+    reveillons: reveillons
+});
+});
+
 app.post('/controle', (req, res) => {
   let login = req.body.login;
   let senha = req.body.senha;
-  
   
   if (login == "book" && senha == "1234") {
     res.redirect('/membros')
@@ -48,20 +106,37 @@ app.post('/controle', (req, res) => {
   };
 });
 
+app.post("/new", (req, res) => {
+  const album = req.body;
+  let escolha = req.body.evento
+
+  if (escolha == 'aniversarios') {
+    aniversarios.push(album);
+  } else if (escolha == 'baladas') {
+      baladas.push(album);
+  } else if (escolha == 'casamentos') {
+    casamentos.push(album);
+  } else if (escolha == 'churrascos') {
+    churrascos.push(album);
+  } else if (escolha == 'festivais') {
+    festivais.push(album);
+  } else if (escolha == 'happyHour') {
+    happyHour.push(album);
+  } else if (escolha == 'malabares') {
+    malabares.push(album);
+  } else if (escolha == 'reveillons') {
+    reveillons.push(album);
+  }   
+
+  res.redirect(escolha);
+});
+
 // app.get("/detalhes/:id", (req, res) => {
 //   const id = req.params.id;
 //   const bookDaGalera = book[id];
 //   res.render("detalhes", {
 //     bookDaGalera,
 //   });
-// });
-
-
-// app.post("/new", (req, res) => {
-//   const pokemon = req.body;
-//   book.push(bookDaGalera);
-//   message = "Informações adiconadas";
-//   res.redirect("/mebros");
 // });
 
 app.listen(port, () =>
