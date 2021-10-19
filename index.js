d6899df97ef93431ad61aa7a3454970ac303d64f
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-10-19 19:20:51 -0300

    commit

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const Sequelize = require("sequelize");
 require('dotenv').config()
-// const multer = require("multer");
-// const morgan = require('morgan');
+const multer = require("multer");
+const morgan = require('morgan');
 
 
-// app.use(express.json());
-// app.use(morgan('dev'));
+app.use(express.json());
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
@@ -35,73 +35,73 @@ const db = knex({
   }  
 );
 
-const imageUpload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-    cb(null, 'image/');
-    },
-    filename: function (req, file, cb) {
-    cb(null, new Date().valueOf() + '_' + file.originalname);
-    }
-  }), 
-});
+// const imageUpload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//     cb(null, 'image/');
+//     },
+//     filename: function (req, file, cb) {
+//     cb(null, new Date().valueOf() + '_' + file.originalname);
+//     }
+//   }), 
+// });
 
-app.post('/image', imageUpload.single('image'), (req, res) => {
-    const { filename, mimetype, size } = req.file;
-    const filepath = req.file.path;
-db
-  .insert({
-        filename,
-        filepath,
-        mimetype,
-        size,
-        })
+// app.post('/image', imageUpload.single('image'), (req, res) => {
+//     const { filename, mimetype, size } = req.file;
+//     const filepath = req.file.path;
+// db
+//   .insert({
+//         filename,
+//         filepath,
+//         mimetype,
+//         size,
+//         })
 
-  .into('image_files')
-  .then(() => res.json({ success: true, filename }))
-  .catch(err => res
-  .json({ 
-      success: false,
-      message: 'upload failed',
-      stack: err.stack,
-      }));
+//   .into('image_files')
+//   .then(() => res.json({ success: true, filename }))
+//   .catch(err => res
+//   .json({ 
+//       success: false,
+//       message: 'upload failed',
+//       stack: err.stack,
+//       }));
       
-      message = "Arquivo enviado com sucesso!"
+//       message = "Arquivo enviado com sucesso!"
 
-        res.render("rota", {
-          message,
-        });
-});
+//         res.render("rota", {
+//           message,
+//         });
+// });
 
-app.get('/image/:filename', async (req, res) => {
-  const imagem = await Image_files.findAll();
-    const { filename } = req.params;
-    db
-      .select('*')
-      .from('image_files')
-      .where({ filename })
-      .then(images => {
-      if (images[0]) {
-        const dirname = path.resolve();
-        const fullfilepath = path.join(dirname, images[0].filepath);
-        return res
-      .type(images[0].mimetype)
-      .sendFile(fullfilepath);
-      }
-      return Promise.reject(new Error('Image does not exist'));
-      })
-      .catch(err => res
-      .status(404)
-      .json({
-        success: false, 
-        message: 'not found', 
-        stack: err.stack,
-      }),
-      );
-      res.render("fotos", {
-        imagem,
-      })
-});
+// app.get('/image/:filename', async (req, res) => {
+//   const imagem = await Image_files.findAll();
+//     const { filename } = req.params;
+//     db
+//       .select('*')
+//       .from('image_files')
+//       .where({ filename })
+//       .then(images => {
+//       if (images[0]) {
+//         const dirname = path.resolve();
+//         const fullfilepath = path.join(dirname, images[0].filepath);
+//         return res
+//       .type(images[0].mimetype)
+//       .sendFile(fullfilepath);
+//       }
+//       return Promise.reject(new Error('Image does not exist'));
+//       })
+//       .catch(err => res
+//       .status(404)
+//       .json({
+//         success: false, 
+//         message: 'not found', 
+//         stack: err.stack,
+//       }),
+//       );
+//       res.render("fotos", {
+//         imagem,
+//       })
+// });
 
 app.get("/eventos", async (req, res) => {
   const eventos_img = await Eventos_img.findAll();
