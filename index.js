d7ef7d51ace7d0ba6eda4b90dd079010d67603d2
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-10-19 18:51:56 -0300

    commit

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const Sequelize = require("sequelize");
 require('dotenv').config()
-const multer = require("multer");
-const morgan = require('morgan');
+// const multer = require("multer");
+// const morgan = require('morgan');
 
 
-app.use(express.json());
-app.use(morgan('dev'));
+// app.use(express.json());
+// app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
@@ -18,119 +18,119 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const Eventos_img = require("./models/eventos");
 const Albuns_ = require("./models/albuns");
-const Image_files = require("./models/image_files")
+// const Image_files = require("./models/image_files")
 
 let message = "";
 
-app.get("/eventos", async (req, res) => {
-  const eventos_img = await Eventos_img.findAll();
+// app.get("/eventos", async (req, res) => {
+//   const eventos_img = await Eventos_img.findAll();
 
-  res.render("eventos", {
-    eventos_img,
-  });
-});
+//   res.render("eventos", {
+//     eventos_img,
+//   });
+// });
 
-const knex = require('knex');
-const db = knex({
-  client: 'postgres',
-  connection: process.env.DATABASE_URL,
-  dialectOptions: {         
-    ssl: {             
-      require: true,             
-      rejectUnauthorized: false         
-    }     
-  }
-  }  
-);
+// const knex = require('knex');
+// const db = knex({
+//   client: 'postgres',
+//   connection: process.env.DATABASE_URL,
+//   dialectOptions: {         
+//     ssl: {             
+//       require: true,             
+//       rejectUnauthorized: false         
+//     }     
+//   }
+//   }  
+// );
 
-const imageUpload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-    cb(null, 'image/');
-    },
-    filename: function (req, file, cb) {
-    cb(null, new Date().valueOf() + '_' + file.originalname);
-    }
-  }), 
-});
+// const imageUpload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//     cb(null, 'image/');
+//     },
+//     filename: function (req, file, cb) {
+//     cb(null, new Date().valueOf() + '_' + file.originalname);
+//     }
+//   }), 
+// });
 
-app.post('/image', imageUpload.single('image'), (req, res) => {
-    const { filename, mimetype, size } = req.file;
-    const filepath = req.file.path;
-db
-  .insert({
-        filename,
-        filepath,
-        mimetype,
-        size,
-        })
+// app.post('/image', imageUpload.single('image'), (req, res) => {
+//     const { filename, mimetype, size } = req.file;
+//     const filepath = req.file.path;
+// db
+//   .insert({
+//         filename,
+//         filepath,
+//         mimetype,
+//         size,
+//         })
 
-  .into('image_files')
-  .then(() => res.json({ success: true, filename }))
-  .catch(err => res
-  .json({ 
-      success: false,
-      message: 'upload failed',
-      stack: err.stack,
-      }));
+//   .into('image_files')
+//   .then(() => res.json({ success: true, filename }))
+//   .catch(err => res
+//   .json({ 
+//       success: false,
+//       message: 'upload failed',
+//       stack: err.stack,
+//       }));
       
-      message = "Arquivo enviado com sucesso!"
+//       message = "Arquivo enviado com sucesso!"
 
-        res.render("rota", {
-          message,
-        });
-});
+//         res.render("rota", {
+//           message,
+//         });
+// });
 
-app.get('/image/:filename', async (req, res) => {
-  const imagem = await Image_files.findAll();
-    const { filename } = req.params;
-    db
-      .select('*')
-      .from('image_files')
-      .where({ filename })
-      .then(images => {
-      if (images[0]) {
-        const dirname = path.resolve();
-        const fullfilepath = path.join(dirname, images[0].filepath);
-        return res
-      .type(images[0].mimetype)
-      .sendFile(fullfilepath);
-      }
-      return Promise.reject(new Error('Image does not exist'));
-      })
-      .catch(err => res
-      .status(404)
-      .json({
-        success: false, 
-        message: 'not found', 
-        stack: err.stack,
-      }),
-      );
-      res.render("fotos", {
-        imagem,
-      })
-});
+// app.get('/image/:filename', async (req, res) => {
+//   const imagem = await Image_files.findAll();
+//     const { filename } = req.params;
+//     db
+//       .select('*')
+//       .from('image_files')
+//       .where({ filename })
+//       .then(images => {
+//       if (images[0]) {
+//         const dirname = path.resolve();
+//         const fullfilepath = path.join(dirname, images[0].filepath);
+//         return res
+//       .type(images[0].mimetype)
+//       .sendFile(fullfilepath);
+//       }
+//       return Promise.reject(new Error('Image does not exist'));
+//       })
+//       .catch(err => res
+//       .status(404)
+//       .json({
+//         success: false, 
+//         message: 'not found', 
+//         stack: err.stack,
+//       }),
+//       );
+//       res.render("fotos", {
+//         imagem,
+//       })
+// });
 
 
-app.get("/albuns/:id", async  (req, res) => { 
-  const eventos_img = await Eventos_img.findByPk(req.params.id);
+// app.get("/albuns/:id", async  (req, res) => { 
+//   const eventos_img = await Eventos_img.findByPk(req.params.id);
 
-  let eventos_ = ["aniversarios", "baladas", "casamentos", "churrascos", "festivais", "happyHour", "malabares","reveillons"];
+//   let eventos_ = ["aniversarios", "baladas", "casamentos", "churrascos", "festivais", "happyHour", "malabares","reveillons"];
 
-  let alb_db = parseInt(req.params.id)
-  alb_db--
+//   let alb_db = parseInt(req.params.id)
+//   alb_db--
 
-  let albuns = await Albuns_.findAll({
-    where: {
-    evento_album: eventos_[alb_db]
-    }
-    });
+//   let albuns = await Albuns_.findAll({
+//     where: {
+//     evento_album: eventos_[alb_db]
+//     }
+//     });
 
-  res.render("albuns", {
-    albuns,
-    eventos_img,
-  });
-});
+//   res.render("albuns", {
+//     albuns,
+//     eventos_img,
+//   });
+// });
 
 
 app.get("/", (req, res) => {
@@ -138,129 +138,129 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/controle", (req, res) => {
-  setTimeout(() => {
-    message = "";
-  }, 1000);
+// app.get("/controle", (req, res) => {
+//   setTimeout(() => {
+//     message = "";
+//   }, 1000);
 
-  res.render("controle", {
-    message
-  });
-});
-
-
-app.get("/criar", async (req, res) => {
-  const album = await Albuns_.findAll();
-
-  setTimeout(() => {
-    message = "";
-  }, 1000);
-
-  res.render("criar", {
-    album,
-    message
-  });
-});
+//   res.render("controle", {
+//     message
+//   });
+// });
 
 
-app.post("/criar", async (req, res) => {
-  const album = await Albuns_.findAll();
-  const { nome_album, nome_autor, evento_album, album_imagem, local_album, data_album } = req.body;
+// app.get("/criar", async (req, res) => {
+//   const album = await Albuns_.findAll();
 
-  const album_a = await Albuns_.create({
-    nome_album,
-    nome_autor,
-    evento_album,
-    album_imagem,
-    local_album,
-    data_album,
-  });
+//   setTimeout(() => {
+//     message = "";
+//   }, 1000);
 
-  message = "Album criado com sucesso!";
+//   res.render("criar", {
+//     album,
+//     message
+//   });
+// });
 
-  try {
-    const albuns_a = await Albuns_.create({
-      nome_album,
-      nome_autor,
-      album_imagem
-    });
 
-  } catch (err) {
-    console.log(err);
-    res.redirect("criar")
-  }
+// app.post("/criar", async (req, res) => {
+//   const album = await Albuns_.findAll();
+//   const { nome_album, nome_autor, evento_album, album_imagem, local_album, data_album } = req.body;
 
-});
+//   const album_a = await Albuns_.create({
+//     nome_album,
+//     nome_autor,
+//     evento_album,
+//     album_imagem,
+//     local_album,
+//     data_album,
+//   });
 
-app.post("/editar/:id", async (req, res) => {
-  const albuns = await Albuns_.findByPk(req.params.id);
-  const album = await Albuns_.findAll();
+//   message = "Album criado com sucesso!";
 
-  const { nome_album, nome_autor, evento_album, album_imagem, local_album, data_album } = req.body;
-  albuns.nome_album = nome_album,
-  albuns.nome_autor = nome_autor,
-  albuns.evento_album = evento_album,
-  albuns.album_imagem = album_imagem,
-  albuns.local_album = local_album,
-  albuns.data_album = data_album
+//   try {
+//     const albuns_a = await Albuns_.create({
+//       nome_album,
+//       nome_autor,
+//       album_imagem
+//     });
 
-  const albunsEditado = await albuns.save();
+//   } catch (err) {
+//     console.log(err);
+//     res.redirect("criar")
+//   }
 
-  message =  "Album editado com sucesso!";
+// });
 
-  res.render("rota", {
-    albuns: albunsEditado,
-    album,
-    message
-  });
-});
+// app.post("/editar/:id", async (req, res) => {
+//   const albuns = await Albuns_.findByPk(req.params.id);
+//   const album = await Albuns_.findAll();
 
-app.get("/deletar/:id", async (req, res) => {
-  const albuns = await Albuns_.findByPk(req.params.id);
-  const album = await Albuns_.findAll();
+//   const { nome_album, nome_autor, evento_album, album_imagem, local_album, data_album } = req.body;
+//   albuns.nome_album = nome_album,
+//   albuns.nome_autor = nome_autor,
+//   albuns.evento_album = evento_album,
+//   albuns.album_imagem = album_imagem,
+//   albuns.local_album = local_album,
+//   albuns.data_album = data_album
 
-  await albuns.destroy();
+//   const albunsEditado = await albuns.save();
 
-  message = "Album excluido com sucesso!";
+//   message =  "Album editado com sucesso!";
 
-  res.render("rota");
+//   res.render("rota", {
+//     albuns: albunsEditado,
+//     album,
+//     message
+//   });
+// });
 
-});
+// app.get("/deletar/:id", async (req, res) => {
+//   const albuns = await Albuns_.findByPk(req.params.id);
+//   const album = await Albuns_.findAll();
 
-app.get("/eventos/:id", async (req, res) => {
-  const album = await Albuns_.findAll();
+//   await albuns.destroy();
 
-  res.render("eventos", {
-    album
-  });
+//   message = "Album excluido com sucesso!";
 
-});
+//   res.render("rota");
 
-app.get("/sobre", (req, res) => {
-  res.render("sobre");
-});
+// });
 
-app.post('/controle', (req, res) => {
-  let login = req.body.login;
-  let senha = req.body.senha;
+// app.get("/eventos/:id", async (req, res) => {
+//   const album = await Albuns_.findAll();
+
+//   res.render("eventos", {
+//     album
+//   });
+
+// });
+
+// app.get("/sobre", (req, res) => {
+//   res.render("sobre");
+// });
+
+// app.post('/controle', (req, res) => {
+//   let login = req.body.login;
+//   let senha = req.body.senha;
   
-  if (login == "book" && senha == "1234") {
-    res.redirect('/criar')
-  } else {
-    message = 'Usuário ou senha inválido'
-    res.redirect('/controle')
-  };
-});
+//   if (login == "book" && senha == "1234") {
+//     res.redirect('/criar')
+//   } else {
+//     message = 'Usuário ou senha inválido'
+//     res.redirect('/controle')
+//   };
+// });
 
-app.get("/fotos/:id", async  (req, res) => { 
-  const albuns = await Albuns_.findAll();
-  const eventos_img = await Eventos_img.findByPk(req.params.id);
+// app.get("/fotos/:id", async  (req, res) => { 
+//   const albuns = await Albuns_.findAll();
+//   const eventos_img = await Eventos_img.findByPk(req.params.id);
 
-  res.render("fotos", {
-    albuns,
-    eventos_img
-  });
-});
+//   res.render("fotos", {
+//     albuns,
+//     eventos_img
+//   });
+// });
 
 app.listen(port, () =>
 console.log(`Servidor rodando em http://localhost:${port}`)
